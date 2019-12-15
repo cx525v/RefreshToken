e1d033baf680e28a3fbd63413bd35641e58a5c96
cx525v <cx525v@gmail.com>
2019-12-15 18:05:28 -0500

    Create AuthGuard.ts

---
+++ AuthGuard.ts
@@ -0,0 +1,14 @@
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate() {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/secret-random-number']);
+    }
+    return !this.authService.isLoggedIn();
+  }
+}
